c9b0d67fe24d2ef197e55418659fc1c9fc81840b
senne-geerts <senne.geerts@student.ucll.be>
2024-10-25 10:43:27 +0200

    adding dummy data organizer

--- back-end/repository/organizer.db.ts
+++ back-end/repository/organizer.db.ts
@@ -1,7 +1,19 @@
 import { Organizer } from "../model/organizer";
 import { User } from "../model/user";
+import { Event } from "../model/event";
+import { Speaker } from "../model/speaker";
 
-const organizers : Organizer[] = [];
+// Dummy User instances
+const user1 = new User({username: "john123", password:'password',  firstName:'John',lastName:'Doe', email: "john@example.com", role: 'organizer' });
+const user2 = new User({username: "jane123", password: "password", firstName:'Jane',lastName:'Doe', email: "jane@example.com", role: 'organizer'});
+const user3 = new User({username: "senne123",password:"password" , firstName:'Senne',lastName:'Geerts', email: "senne@example.com", role: 'organizer'});
+
+// Initialize organizers array with dummy data
+const organizers: Organizer[] = [
+    new Organizer({ user: user1, companyName: "John's Eventing" }),
+    new Organizer({ user: user2, companyName: "Jane's Business" }),
+    new Organizer({ user: user3, companyName: "Senne's Club" })
+];
 
 const createOrganizer = ({
     user,
