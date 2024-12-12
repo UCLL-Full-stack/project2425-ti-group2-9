5a8c016b2cb9fb7d20545adfd17d547daaca9c22
senne-geerts <senne.geerts@student.ucll.be>
2024-12-12 10:02:37 +0100

    accapting auth for speakerservice and event (frontend)

--- front-end/services/EventService.ts
+++ front-end/services/EventService.ts
@@ -1,8 +1,11 @@
 const createEvent = async (formData: any) => {
+    const user =  localStorage.getItem("loggedInUser")
+    const token = user ? JSON.parse(user).token : null;
     return fetch(process.env.NEXT_PUBLIC_API_URL+'/events',{
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            Authorization:  `Bearer ${token}`
         },
         body: JSON.stringify(formData)
     })
