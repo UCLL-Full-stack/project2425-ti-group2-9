cf09b462d6ea067a0dab23f51287567e2660221c
senne-geerts <senne.geerts@student.ucll.be>
2024-12-19 20:25:41 +0100

    searching in events

--- front-end/components/events/EventOverviewTable.tsx
+++ front-end/components/events/EventOverviewTable.tsx
@@ -84,7 +84,7 @@ const EventOverviewTable: React.FC<Props> = ({ events }: Props) => {
                   <button
                     className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                     onClick={(e) => {
-                      e.stopPropagation(); // Prevent row click event
+                      e.stopPropagation(); 
                       handleAttending(event);
                     }}
                     data-testid={`attend-button-${index}`}
